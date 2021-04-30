b48ff14471e1d4370529b50742a6aafc5c56015a
Ehimah Obuse <ehimahobuse@gmail.com>
2021-05-01 00:17:24 +0200

    Create deploy-envs.sh

---
+++ deploy-envs.sh
@@ -0,0 +1,11 @@
+export IMAGE_NAME=ecsdemo-redisgeo
+export IMAGE_VERSION=latest
+
+
+export AWS_DEFAULT_REGION=eu-west-1
+export AWS_ECS_CLUSTER_NAME=default
+export AWS_VIRTUAL_HOST=ec2-54-74-32-70.eu-west-1.compute.amazonaws.com
+
+export LETSENCRYPT_HOST=$AWS_VIRTUAL_HOST
+export LETSENCRYPT_EMAIL=ehimahobuse@gmail.com
+
